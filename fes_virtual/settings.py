52c2f2a6887d4c5157d4f8c1c3e51ad98fa2454b
TOM BAÑADOS <tbanados@uc.cl>
2021-10-02 19:11:31 -0300

    key hidden

--- fes_virtual/settings.py
+++ fes_virtual/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from key import key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k9+vf4ncev5+l0u32ah3y+q_b=ylclq5^qsa)8(2uoq#@3*99j'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
