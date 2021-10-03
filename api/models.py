add1e378c7eb408e295208693d07c6c9814bd874
TOM BAÑADOS <tbanados@uc.cl>
2021-10-02 21:16:34 -0300

    created first model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    prompt = models.CharField(max_length=128, unique=False)
+    answer = models.CharField(max_length=512)
